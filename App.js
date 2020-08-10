e5be06a7766061001a69b9845a088e3b5d8b5d63
magicsli <2544510157@qq.com>
2020-08-10 22:44:21 +0800

    init project

---
+++ App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  Dimensions,
+  Button
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView  style={styles.mianView}>
+        <View>
+          <Text> 
+              当前屏幕宽度为: {Dimensions.get('window').width}; {'\n'}
+              当前屏幕高度为: {Dimensions.get('window').height};{'\n'}
+              当前屏幕分辨率为: {Dimensions.get('window').scale};{'\n'}
+          </Text>
+
+        
+        </View>
+      </SafeAreaView> 
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  mianView: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'red',
+    borderLeftColor: '#ccc',
+    borderLeftWidth: 20,
+    borderStyle: 'dashed',
+    
+  }
+})
+
+export default App;
