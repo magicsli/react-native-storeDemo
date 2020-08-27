5eedd5cfa255fdf142f6565152e0a18f6e4f0dd0
magicsli <2544510157@qq.com>
2020-08-27 20:18:24 +0800

    test

--- App.js
+++ App.js
@@ -7,39 +7,23 @@
  */
 
 import React from 'react';
+
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
   StatusBar,
-  Image,
-  Dimensions,
-  Button
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Index from '@/page/index'
-import Scroll from '@/page/scorll'
+import Index from '@/page/index';
+import Scroll from '@/page/scorll';
 
 const Stack = createStackNavigator();
 
-
-
 const App: () => React$Node = () => {
   return (
-    <View style={styles.mianView}>
       <NavigationContainer>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView style={styles.mianView}>
@@ -49,7 +33,6 @@ const App: () => React$Node = () => {
           </Stack.Navigator>
         </SafeAreaView>
       </NavigationContainer>
-    </View>
   );
 };
 
